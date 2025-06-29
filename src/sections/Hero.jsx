01c4f606095d50efc3e923f24c442cc88a7ed52e
unknown <challoufrayen09@gmail.com>
2025-06-29 07:01:40 +0100

    first commit

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section id="hero" className='relative overflow-hidden'>
+        <div className='absolute top-0 left-0 z-10'>
+                <img src="/images/bg.png" alt="background" />
+         </div>
+         <div className="hero-layout">
+            {/*left hero content*/}
+            {/*right 3d  layout*/}
+
+         </div>
+    </section>
+  )
+}
+
+export default Hero
