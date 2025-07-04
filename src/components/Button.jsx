686e6e859637695b89d1dbcc80597bda63850bed
unknown <challoufrayen09@gmail.com>
2025-07-04 05:20:32 +0100

    added the animated counters section

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
 
-const Button = ({text,className}) => {
+const Button = ({text,className,id}) => {
   return (
     <a href="
-    " className={` ${className ?? '' } cta-wrapper`}>
+
+    " 
+    onClick ={
+      (e)=>{
+        e.preventDefault();
+        const target =document.getElementById('counter')
+        if (target && id){
+          const offset =window.innerHeight *0.15;
+          const top = target.getBoundingClientRect().top +window.scrollY -offset;
+          window.scrollTo ({top,behavior:'smooth'})
+          
+
+        }
+  
+        }}
+    className={` ${className ?? '' } cta-wrapper`}>
         <div className="cta-button group">
             <div className="bg-circle"/>
                 <p className="text">{text}</p>
